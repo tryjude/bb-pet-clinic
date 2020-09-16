0b21eebc1696856df975a2cb8a1d86bf3f0d221e
Jude <tryjude@gmail.com>
2020-09-16 11:38:03 -0400

    Generate spring intializer and setup project

---
+++ src/main/java/guru/spring/bbpetclinic/BbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.spring.bbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/spring/bbpetclinic/BbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.bbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
